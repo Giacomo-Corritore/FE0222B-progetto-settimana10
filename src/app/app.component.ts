9d3eb45571b8e02b567b497fc4289227c339744b
Giacomo-Corritore <giacomo.corritore@gmail.com>
2022-05-08 16:34:59 +0200

    commit progetto settimana10

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'FE0222B-progetto-settimana10';
+  title = 'FE07B_progetto_settimana9';
 }
